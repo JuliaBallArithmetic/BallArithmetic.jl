cb97d92829a7e52dfefad9b49160c4a4bcdc2c08
Documenter.jl <documenter@juliadocs.github.io>
2023-05-03 20:37:42 +0000

    build based on ba473de

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BallArithmetic","category":"page"},{"location":"#BallArithmetic","page":"Home","title":"BallArithmetic","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BallArithmetic.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BallArithmetic]","category":"page"}]
+}
