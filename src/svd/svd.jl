a35a4cfc0f3e78ba9408146474ccc44989098dcf
Isaia Nisoli <nisoli@im.ufrj.br>
2023-05-16 16:51:34 -0300

    Implemented the suggestions by  Luca

--- src/svd/svd.jl
+++ src/svd/svd.jl
@@ -1,6 +1,6 @@
 
 function upper_abs(A::BallMatrix)
-    return abs.(A.c)+A.r
+    return abs.(A.c) + A.r
 end
 
 # we use Perron theory here: if for two matrices with B positive 
@@ -8,7 +8,7 @@ end
 # Wielandt's theorem 
 # https://mathworld.wolfram.com/WielandtsTheorem.html
 
-function upper_bound_norm(A::BallMatrix{T}; iterates = 10) where {T}
+function upper_bound_norm(A::BallMatrix{T}; iterates=10) where {T}
     m, k = size(A)
     x_old = ones(m)
     x_new = x_old
@@ -29,39 +29,43 @@ function upper_bound_norm(A::BallMatrix{T}; iterates = 10) where {T}
 end
 
 function svdbox(A::BallMatrix{T}) where {T}
-    svdA  = svd(A.c)
-    
-    U = BallMatrix(svdA.U, zeros(size(svdA.U)))
-    Vt = BallMatrix(svdA.Vt, zeros(size(svdA.Vt)))
-    Σ = BallMatrix(Diagonal(svdA.S), Diagonal(zeros(size(svdA.S))))
-    V = BallMatrix(svdA.V, zeros(size(svdA.V)))
+    svdA = svd(A.c)
 
-    E = U*Σ*Vt - A
-    normE = sqrt(upper_bound_norm(E))
-    @info "E" normE
-    
-    F = Vt*V-I
-    normF = sqrt(upper_bound_norm(F))
-    @info normF
+    U = BallMatrix(svdA.U)
+    Vt = BallMatrix(svdA.Vt)
+    Σ = BallMatrix(Diagonal(svdA.S))
+    V = BallMatrix(svdA.V)
 
-    G = U'*U-I
-    normG = sqrt(upper_bound_norm(G))
-    @info normG
+    E = U * Σ * Vt - A
+    normE = sqrt_up(upper_bound_norm(E))
+    @debug "norm E" normE
+
+    F = Vt * V - I
+    normF = sqrt_up(upper_bound_norm(F))
+    @debug "norm F" normF
+
+    G = U' * U - I
+    normG = sqrt_up(upper_bound_norm(G))
+    @debug "norm G" normG
 
     @assert normF < 1 "It is not possible to verify the singular values with this precision"
     @assert normG < 1 "It is not possible to verify the singular values with this precision"
 
-    @info T
-    svdbounds_up = setrounding(T, RoundUp) do
-        return [(σ+normE)/(1-normF)*(1-normG) for σ in svdA.S]
-    end
-    
+    den_down = @up (1.0 + normF) * (1.0 + normG)
+    den_up = @down (1.0 - normF) * (1.0 - normG)
+
     svdbounds_down = setrounding(T, RoundDown) do
-        return [(σ-normE)/(1+normF)*(1+normG) for σ in svdA.S]
+        [(σ - normE) / den_down for σ in svdA.S]
     end
-    
-    midpoints = (svdbounds_down+svdbounds_up)/2
-    rad = svdbounds_up-midpoints
+
+    svdbounds_up = setrounding(T, RoundUp) do
+        [(σ + normE) / den_up for σ in svdA.S]
+    end
+
+    midpoints = (svdbounds_down + svdbounds_up) / 2
+    rad = setrounding(T, RoundUp) do
+        [max(svdbounds_up[i] - midpoints[i], midpoints[i] - svdbounds_down[i]) for i in 1:length(midpoints)]
+    end
 
     return [Ball(midpoints[i], rad[i]) for i in 1:length(midpoints)]
 end
