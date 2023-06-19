76e1a25343177ad86225f80db2c4476d924a2e38
Isaia Nisoli <nisoli@im.ufrj.br>
2023-06-19 18:37:11 -0300

    Added product and inverse test

--- test/test_ball/test_ball.jl
+++ test/test_ball/test_ball.jl
@@ -25,4 +25,20 @@
     @test all(in.(lower, bsum))
     @test all(in.(higher, bsum))
 
+    iprod = iv .* iw
+    lower = [x.lo for x in iprod]
+    higher = [x.hi for x in iprod]
+
+    bprod = bv .* bw
+    @test all(in.(lower, bprod))
+    @test all(in.(higher, bprod))
+
+    x = Ball(1.0, 1/4) #interval [3/4, 5/4]
+    t = inv(x) # the inverse is [4/5, 4/3]
+    @test 4/5 ∈ t
+    @test 4/3 ∈ t
+    
+
+    x = Ball(rand()+im*rand())
+    @test 1.0 ∈ x*inv(x)
 end
