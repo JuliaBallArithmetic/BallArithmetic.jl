db1a6c7cb8b35ae9b436b6e1eebf6d35182f66bf
Isaia Nisoli <nisoli@im.ufrj.br>
2023-11-30 12:19:40 -0300

    Added BallArray parametric type

--- src/BallArithmetic.jl
+++ src/BallArithmetic.jl
@@ -44,6 +44,7 @@ export Ball, BallMatrix, ±, mid, rad
 include("rounding.jl")
 include("ball.jl")
 include("matrix.jl")
+include("array.jl")
 include("norm_bounds/rigorous_norm.jl")
 include("norm_bounds/rigorous_opnorm_bounds.jl")
 include("eigenvalues/gev.jl")
