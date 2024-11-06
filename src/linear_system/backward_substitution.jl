48df32ed7ea3d7640449a66aab6740092895e683
Isaia Nisoli <nisoli@im.ufrj.br>
2024-11-06 11:58:20 -0300

    Added Backward Substitution for matrices... not working

--- src/linear_system/backward_substitution.jl
+++ src/linear_system/backward_substitution.jl
@@ -17,3 +17,21 @@ function backward_substitution(A::BallMatrix, v::BallVector)
     end
     return sol
 end
+
+function backward_substitution(A::BallMatrix, B::BallMatrix)
+    @assert istriu(A.c) && istriu(A.r)
+
+    m, k = size(A)
+    @assert m == k
+
+    sol = fill(Ball(0.0), size(B))
+
+    for i in m:-1:1
+        sol[i, :] = B[i, :]
+        for j in m:-1:(i + 1)
+            sol[i] -= A[i, j] * sol[j, :]
+        end
+        sol[i, :] /= Ball(A[i, i])
+    end
+    return sol
+end
