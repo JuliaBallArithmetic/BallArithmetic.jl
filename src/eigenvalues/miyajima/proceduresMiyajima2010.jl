20f82857a8a91ea5db863a8636684512651f9ee8
Isaia Nisoli <nisoli@im.ufrj.br>
2025-10-25 08:59:21 -0300

    Adding Miyajima procedures 2010

---
+++ src/eigenvalues/miyajima/proceduresMiyajima2010.jl
@@ -0,0 +1,20 @@
+function _encR1ci(A, tildeX, tildeD, Y, Zrl, Zru, Zil, Ziu)
+    Crl, Cru, Cil, Ciu = _cprod(A, tildeX)
+    Vrl, Vru, Vil, Viu = _ciprod_prime(Zrl, Zru, Zil, Ziu, tildeD)
+    Url, Uil = setrounding(T, RoundDown) do
+        Url = Crl - Vru
+        Uil = Cil - Viu
+        return Url, Uil
+    end
+    Uru, Uiu = setrounding(T, RoundUp) do
+        Uru = Cru - Vrl
+        Uiu = Ciu - Vil
+        return Uru, Uiu
+    end
+    return _ciprod(Y, Url, Uru, Uil, Uiu)
+end
+
+function _encR1ccr(A, tildeX, tildeD, Y, Zc, Zr)
+    Crl, Cru, Cil, Ciu = _cprod(A, tildeX)
+    Vrl, Vru, Vil, Viu = _ccrprod_prime(Zc, Zr, tildeD)
+end
